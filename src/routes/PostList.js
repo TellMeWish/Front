6bee8394bc4847d0b0fcfe675cc6360757b59bd1
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-07-21 21:52:25 +0900

    글목록페이지 UI 및 무한스크롤

--- src/routes/PostList.js
+++ src/routes/PostList.js
@@ -4,23 +4,22 @@ import styled from "styled-components";
 import axios from "axios";
 import "../css/PostList.css";
 let Post = styled.div`
-  width: 800px;
+  width: 350px;
   height: 400px;
   padding: 20px;
   background: var(--color-light-green);
-  margin-top: 200px;
   display: flex;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   img {
-    width: 400px;
-  }
-  div {
-    padding: 10px;
+    width: 300px;
+    height: 200px;
   }
 `;
 
 function PostList() {
-  const [items, setItems] = useState();
+  const [items, setItems] = useState([]);
   const [page, setPage] = useState(0);
   const [loading, setLoading] = useState(false);
 
@@ -29,21 +28,18 @@ function PostList() {
   const getItems = useCallback(async () => {
     setLoading(true);
     await axios
-      .get(`http://13.209.145.95:8081/post/postList?page=${page}&size=4`)
+      .get(`http://13.209.145.95:8081/post/postList?page=${page}&size=9`)
       .then((res) => {
         if (items) {
           setItems((prevState) => [...prevState, ...res.data.content]);
-          console.log(page);
-        } else {
-          setItems([...res.data.content]);
-          console.log(page);
-        }
+        } else setItems((prevState) => [...prevState, ...res.data.content]);
       })
       .catch(() => {
         console.log("실패함");
       });
     setLoading(false);
   }, [page]);
+
   useEffect(() => {
     getItems();
   }, [getItems]);
@@ -51,31 +47,54 @@ function PostList() {
   useEffect(() => {
     // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
     if (inView && !loading) {
-      setPage(page + 1);
+      setTimeout(() => {
+        setPage((prevState) => prevState + 1);
+      }, 3000);
     }
-  }, [inView, loading]);
+  }, [inView]);
 
-  console.log(items);
   return (
-    <div style={{ background: "var(--color-skin)" }}>
+    <div style={{ background: "var(--color-skin)", width: "100%" }}>
       <div className="postList">
         {items?.map((item, i) => {
-          if (item)
-            return items.length - 1 == i && page <= items.length / 3 ? (
-              <Post ref={ref}>
+          if (item) {
+            return items.length - 1 == i ? (
+              <Post className="hi" ref={ref}>
                 <img src={item.files[0] ? item.files[0] : process.env.PUBLIC_URL + "/img/noimage.png"}></img>
-                <div>제목 : {item.title}</div>
+                <div className="textBox">
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <div className="postTitle">제목 : {item.title}</div>
+                  </div>
+                  {item.isCompleted ? <div>(모집 완료)</div> : <div>(모집 중)</div>}
+                  <div style={{ display: "flex", marginTop: "50px" }}>
+                    <div>❤️ : {item.likeCount}</div>
+                    <div style={{ marginLeft: "10px" }}>조회수 : {item.viewCount}</div>
+                    <div style={{ marginLeft: "10px" }}></div>
+                  </div>
+                  <div style={{ marginTop: "5px" }}>시간 : {new Date(item.createdAt).toLocaleString("ko-KR", { timeZone: "Asia/Seoul" })}</div>
+                </div>
               </Post>
             ) : (
               <Post>
                 <img src={item.files[0] ? item.files[0] : process.env.PUBLIC_URL + "/img/noimage.png"}></img>
-                <div>제목 : {item.title}</div>
+                <div className="textBox">
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <div className="postTitle">제목 : {item.title}</div>
+                  </div>
+                  {item.isCompleted ? <div>(모집 완료)</div> : <div>(모집 중)</div>}
+                  <div style={{ display: "flex", marginTop: "50px" }}>
+                    <div>❤️ : {item.likeCount}</div>
+                    <div style={{ marginLeft: "10px" }}>조회수 : {item.viewCount}</div>
+                    <div style={{ marginLeft: "10px" }}></div>
+                  </div>
+                  <div style={{ marginTop: "5px" }}>시간 : {new Date(item.createdAt).toLocaleString("ko-KR", { timeZone: "Asia/Seoul" })}</div>
+                </div>
               </Post>
             );
+          }
         })}
       </div>
     </div>
   );
 }
-
 export default PostList;
